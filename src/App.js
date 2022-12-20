5b7c0f7e8180a4db3387d687ba467b662ed3995b
Prashant Shekhar Mishra <prashant_mishra2@persistent.com>
2022-12-20 15:54:46 +0530

    project ready

--- src/App.js
+++ src/App.js
@@ -1,24 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
-
+import Bottombar from './components/bottombar';
+import NavBar from './components/navbar';
+import { BrowserRouter,Routes,Route} from 'react-router-dom';
+import X99 from './components/x99Mark';
+import Cart from './components/cart';
+import Panel4 from './components/Headphone';
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <BrowserRouter>
+        <NavBar />
+          <Routes>
+            <Route path='/' element={<X99/>}/>
+            <Route path='/headphones'  element={<Panel4/>}/>
+            <Route path='/speakers' element={<Panel4/>}/> 
+            <Route path='/earphones' element={<Panel4/>}/>
+            <Route path='/cart' element={<Cart/>} />
+          </Routes>   
+      </BrowserRouter>
+      <Bottombar />
+
+    </>
   );
 }
 
